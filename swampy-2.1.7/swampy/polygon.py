cb98429dff1dea072a89302ce888592a6064543f
Andrew Clark <andrewclark.ku@gmail.com>
2017-01-10 22:33:26 -0600

    updated polygon.py

--- swampy-2.1.7/swampy/polygon.py
+++ swampy-2.1.7/swampy/polygon.py
@@ -9,6 +9,9 @@ def square(t, length):
 		lt(t) 
 
 def polyline(t, n, length, angle):
+	"""Draw n line segments with the given length
+	and angle (in degrees) between them. t is a turtle.
+	"""
 	for i in range(n):
 		fd(t, length) 
 		lt(t, angle)
